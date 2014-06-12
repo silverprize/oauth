64c6a881fd9c42b35f7b43843618b45c43ab1f9b
이은 <tfeslee@hancom.com>
2014-06-12 10:09:45 +0900

    get file list and download file from google drive.

--- google-drive/src/main/java/com/silverprize/GDriveClient.java
+++ google-drive/src/main/java/com/silverprize/GDriveClient.java
@@ -1,40 +1,107 @@
 package com.silverprize;
 
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpResponse;
 import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.IOUtils;
+import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.drive.Drive;
-import com.google.api.services.drive.model.FileList;
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.ChildList;
+import com.google.api.services.drive.model.ChildReference;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.security.GeneralSecurityException;
-import java.util.Arrays;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 
 public class GDriveClient {
-    public static void main(String args[]) throws IOException, GeneralSecurityException {
-        HttpTransport transport = GoogleNetHttpTransport.newTrustedTransport();
-        GoogleClientSecrets.Details details = new GoogleClientSecrets.Details();
-        details.setClientId("627339111893-vbnqjfvohhfuk2r431jkii7jpvu31mab.apps.googleusercontent.com");
-        details.setClientSecret("M-Jm9FN00OT8Eqb0luLo7RD2");
-        details.setRedirectUris(Arrays.asList("urn:ietf:wg:oauth:2.0:oob", "http://localhost"));
-        details.setAuthUri("https://accounts.google.com/o/oauth2/auth");
-        details.setTokenUri("https://accounts.google.com/o/oauth2/token");
-        details.setFactory(GsonFactory.getDefaultInstance());
-        GoogleClientSecrets clientSecrets = new GoogleClientSecrets();
-        clientSecrets.setInstalled(details);
 
-        GoogleCredential credential = new GoogleCredential.Builder()
-                .setTransport(transport)
-                .setClientSecrets(new GoogleClientSecrets().setInstalled(details))
-                .build();
-        Drive drive = new Drive.Builder(transport, GsonFactory.getDefaultInstance(), credential)
-                .setApplicationName("API Project")
-                .build();
-        Drive.Files files = drive.files();
-        Drive.Files.List list = files.list();
-        FileList fileList = list.execute();
-        System.out.println(fileList.getKind());
+    private static final String FOLDER = "application/vnd.google-apps.folder";
+
+    private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+
+    /** Directory to store user credentials. */
+    private static final java.io.File DATA_STORE_DIR = new java.io.File(System.getProperty("user.home"), ".store/google_service");
+
+    public static void main(String args[]) throws Exception {
+        HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+        Credential credential = authorize();
+        Drive drive = new Drive.Builder(httpTransport, JSON_FACTORY, credential).setApplicationName("API Project").build();
+        ChildList childList = drive.children().list("root").execute();
+        for (ChildReference ref : childList.getItems()) {
+            com.google.api.services.drive.model.File gdFile = drive.files().get(ref.getId()).execute();
+            if (!FOLDER.equals(gdFile.getMimeType())) {
+                if (!Boolean.TRUE.equals(gdFile.getExplicitlyTrashed())) {
+                    File downloadFile = downloadFile(drive, gdFile);
+                    if (downloadFile != null && downloadFile.exists()) {
+                        System.out.println("Success to download.");
+                    }
+                }
+            }
+        }
     }
+
+    /** Authorizes the installed application to access user's protected data. */
+    private static Credential authorize() throws Exception {
+        // load client secrets
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY,
+                new InputStreamReader(GDriveClient.class.getResourceAsStream("/client_secrets.json")));
+        if (clientSecrets.getDetails().getClientId().startsWith("Enter")
+                || clientSecrets.getDetails().getClientSecret().startsWith("Enter ")) {
+            System.out.println(
+                    "Enter Client ID and Secret from https://code.google.com/apis/console/?api=drive "
+                            + "into client_secrets.json");
+            System.exit(1);
+        }
+        // set up authorization code flow
+        HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                httpTransport, JSON_FACTORY, clientSecrets, DriveScopes.all())
+                .setDataStoreFactory(new FileDataStoreFactory(DATA_STORE_DIR))
+                .build();
+        // authorize
+        return new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("silver");
+    }
+
+    /**
+     * Download a file's content.
+     *
+     * @param service Drive API service instance.
+     * @param gdFile Drive File instance.
+     * @return InputStream containing the file's content if successful,
+     *         {@code null} otherwise.
+     */
+    private static File downloadFile(Drive service, com.google.api.services.drive.model.File gdFile) {
+        if (gdFile.getDownloadUrl() != null && gdFile.getDownloadUrl().length() > 0) {
+            try {
+                HttpResponse resp =
+                        service.getRequestFactory().buildGetRequest(new GenericUrl(gdFile.getDownloadUrl()))
+                                .execute();
+                File dest = new File(gdFile.getTitle());
+                OutputStream out = new BufferedOutputStream(new FileOutputStream(dest));
+                IOUtils.copy(resp.getContent(), out);
+                out.close();
+                return dest;
+            } catch (IOException e) {
+                // An error occurred.
+                e.printStackTrace();
+                return null;
+            }
+        } else {
+            // The file doesn't have any content stored on Drive.
+            return null;
+        }
+    }
+
 }
